f6238a74b8629b3ab8c70228dd619040474576c7
florian1345 <florian1345@gmx.de>
2021-12-26 01:33:16 +0100

    rest of godot approach
    switching to maui

--- app/Test.cs
+++ app/Test.cs
@@ -10,9 +10,9 @@ public class Test : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var engine = GetNode("Engine");
-		var label = GetNode("CenterContainer").GetNode("VBoxContainer").GetNode<Label>("Label");
-		label.Text = engine.Call("test").ToString();
+		// var engine = GetNode("Engine");
+		// var label = GetNode("CenterContainer").GetNode("VBoxContainer").GetNode<Label>("Label");
+		// label.Text = engine.Call("test").ToString();
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
